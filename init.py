78ea9c60e13fc522b008c18192590225acd823a2
Alice Bevan-McGregor <alice.mcgregor@me.com>
2010-11-21 17:20:57 -0800

    Clear the DB on run and data update.

--- init.py
+++ init.py
@@ -4,7 +4,12 @@
 from model import *
 
 if __name__ == '__main__':
+    # Clear data.
+    
+    Feed.drop_collection()
+    Story.drop_collection()
+    
     # Seed data.
-    Feed(name="eff-official", title="The Electronic Frontier Foundation", site="http://www.eff.org/", url="https://www.eff.org/rss/updates.xml", search=FeedSearch(attribute="title", value="TSA")).save()
-    Feed(name="schneier-official", title="Schneier on Security", site="http://www.schneier.com/", url="http://feeds.feedburner.com/schneier/excerpts", search=FeedSearch(attribute="title", value="TSA")).save()
+    Feed(name="eff-official", title="The Electronic Frontier Foundation", site="http://www.eff.org/", url="https://www.eff.org/rss/updates.xml", search=FeedSearch(attribute="title", value="TSA"), enabled=True).save()
+    Feed(name="schneier-official", title="Schneier on Security", site="http://www.schneier.com/", url="http://feeds.feedburner.com/schneier/excerpts", search=FeedSearch(attribute="title", value="TSA"), enabled=True).save()
     Feed(name="tsa-official", title="The TSA Blog", site="http://www.tsa.gov/", url="http://blog.tsa.gov/feeds/posts/default?alt=rss", enabled=True).save()
